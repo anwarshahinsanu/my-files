4d9de0e16116a3d3b702decf4f3c7e156ac7ef9d
anwarshahinsanu <98008498+anwarshahinsanu@users.noreply.github.com>
2023-02-03 23:29:34 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,9 @@
+import random
+fortune = random.randint(1,3)
+print(fortune)
+if fortune == 1:
+    print("Today will be a lucky day")
+elif fortune == 2:
+    print("You will meet someone new and interesting")
+elif fortune == 3:
+    print("Watch out for obstacles in your path")
